f1723c1c7ff8de33f45991e5b2338ded9295f60f
0521CI_JEC <21ci0521@jec.ac.jp>
2022-07-15 15:30:58 +0900

    EnemyMove更新 ※まだ動きません

--- Assets/Enemy/Script/EnemyControll.cs
+++ Assets/Enemy/Script/EnemyControll.cs
@@ -15,25 +15,37 @@ public class EnemyControll : MonoBehaviour
     [SerializeField] GameObject LookEye;
     [SerializeField] float MoveSpeed;
     [SerializeField] Rigidbody EnemyRigidBody;
+
+    //後に別スクリプトに移行予定
+    private float rayDistance;
+    private GameObject NearObject;
+    [SerializeField] Transform[] MoveTransform;
     void Start()
     {
+        rayDistance = 1.0f;
     }
-    private void FixedUpdate()
+    private void FixedUpdate() //後に別スクリプトに移行予定
     {
         var Directoin = transform.forward;
+        Vector3 rayPosition = transform.position + new Vector3(0.0f, 0.0f, 0.0f);
         RaycastHit hit;
-        Debug.DrawRay(gameObject.transform.position,Directoin* 6, Color.blue, 0.1f);
+        Debug.DrawRay(rayPosition, Directoin * rayDistance*6, Color.red*6);
         if (Physics.Raycast(gameObject.transform.position, Directoin, out hit, 6.0f))
         {
             EnemyStatus._EnemyState = EnemyStatus.EnemyState.MOVE;
         }
         else
         {
+            NearObject = MovingPositionSearch(this.gameObject, "MOVEPOSITON");
             EnemyStatus._EnemyState = EnemyStatus.EnemyState.IDOL;
-            this.transform.LookAt(LookEye.transform);
-            /* this.transform.DORotate(Vector3.up * 90f, 1f).OnComplete(() =>
-             {//実行完了時のコールバック
-             });*/
+            this.transform.DOMove(NearObject.transform.position, 3f)
+                .OnComplete(() =>
+            {//実行完了時のコールバック
+                #region 未処理
+                this.transform.rotation = NearObject.transform.rotation;
+                EnemyStatus._EnemyState = EnemyStatus.EnemyState.MOVE;
+                #endregion
+            });
         }
     }
     void Update()
@@ -53,8 +65,27 @@ public class EnemyControll : MonoBehaviour
                 break;
         }
     }
-    void LookStage()
+    GameObject MovingPositionSearch(GameObject NowObj, string TagName)
     {
-        this.transform.LookAt(LookEye.transform);
+        float RangeWithObject = 0;
+        float NearDistance = 0;
+
+        GameObject targetObject = null;
+
+        foreach (GameObject @object in GameObject.FindGameObjectsWithTag(TagName))
+        {
+            //自身と取得したオブジェクトとの距離取得
+            RangeWithObject = Vector3.Distance(@object.transform.position, NowObj.transform.position);
+
+            //オブジェクトに距離が近いか、距離0であればオブジェクト名を取得
+            //一時変数に距離を格納
+            if (NearDistance == 0 || NearDistance > RangeWithObject)
+            {
+                NearDistance = RangeWithObject;
+                targetObject = @object;
+            }
+        }
+        //最も近かったオブジェクトを返す
+        return targetObject;
     }
 }
